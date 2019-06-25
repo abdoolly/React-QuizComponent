178049f2714c9ef58b6045a81c17976004976207
Abdallah Gamal <abdoolly@gmail.com>
2019-06-25 05:17:08 +0200

    finished module 6 successfully

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -3,9 +3,38 @@ import QuizQuestionButton from './QuizQuestionButton';
 
 class QuizQuestion extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            incorrectAnswer: false
+        }
+    }
+
     handleClick(buttonText) {
-        if (buttonText === this.props.quiz_question.answer)
-            return this.props.showNextQuestionHandler();
+        if (buttonText !== this.props.quiz_question.answer)
+            return this.changeAnswerToIncorrect(true);
+
+        this.changeAnswerToIncorrect(false);
+        return this.props.showNextQuestionHandler();
+    }
+
+    changeAnswerToIncorrect(isIncorrectAnswer) {
+        this.setState({
+            incorrectAnswer: isIncorrectAnswer
+        });
+    }
+
+    showErrorIfIncorrect() {
+        return this.state.incorrectAnswer ? <p className="error">Sorry, that's not right</p> : null;
+    }
+
+    getQuizQuestionAnswers() {
+        return this.props.quiz_question.answer_options.map(
+            (answer_option, index) =>
+                (
+                    <QuizQuestionButton clickHandler={this.handleClick.bind(this)} key={`${index}`} button_text={answer_option} />
+                )
+        )
     }
 
     render() {
@@ -16,18 +45,14 @@ class QuizQuestion extends Component {
                 </section>
                 <section className="buttons">
                     <ul>
-                        {this.props.quiz_question.answer_options.map(
-                            (answer_option, index) =>
-                                (
-                                    <QuizQuestionButton clickHandler={this.handleClick.bind(this)} key={`${index}`} button_text={answer_option} />
-                                )
-                        )
-                        }
+                        {this.getQuizQuestionAnswers()}
                     </ul>
                 </section>
+                {this.showErrorIfIncorrect()}
             </main>
         )
     }
+
 }
 
 export default QuizQuestion
